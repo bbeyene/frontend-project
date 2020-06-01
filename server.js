a57f96c424a57f114c8c5c0b353c1c64b6971252
bbeyene <bbeyene@gmail.com>
2020-06-01 08:12:06 +0000

    added support for week with day queries

--- server.js
+++ server.js
@@ -35,28 +35,31 @@ app.get('/data/*', function (req, res) {
   let locationtext = splitPath[4].split('%20').join(' ');
   let lane = splitPath[5];
   let day = splitPath[6];
+  let week = parseInt(splitPath[7]) - 1;
 
-  if(day === "sunday") { start = "2011-10-02"; end = "2011-10-03"; }
-  else if(day === "monday") { start = "2011-10-03"; end = "2011-10-04"; }
-  else if(day === "tuesday") { start = "2011-10-04"; end = "2011-10-05"; }
-  else if(day === "wednesday") { start = "2011-10-05"; end = "2011-10-06"; }
-  else if(day === "thursday") { start = "2011-10-06"; end = "2011-10-07"; }
-  else if(day === "friday") { start = "2011-10-07"; end = "2011-10-08"; }
-  else if(day === "saturday") { start = "2011-10-08"; end = "2011-10-09"; }
-  
+  if(day === "sunday") day = 0;
+  else if(day === "monday") day = 1;
+  else if(day === "tuesday") day = 2
+  else if(day === "wednesday") day = 3
+  else if(day === "thursday") day = 4;
+  else if(day === "friday") day = 5;
+  else if(day === "saturday") day = 6;
+
+  let dateZero = new Date("September 18, 2011");
+  let startDate = addDays(dateZero, (week * 7) + day);
+  let endDate = addDays(startDate, 1);
+	
   const query1 = "SELECT detectorid FROM detectors_by_highway WHERE direction = ? and highwayname = ? and locationtext = ? and lane = ?";
   const params1 = [direction, highwayname, locationtext, lane];
   client.execute(query1, params1, {prepare: true})
 	.then(result => { return result.rows[0]; } )
 	.then(row => {
-			console.log(row.detectorid);
   			let detectorid = row.detectorid;
-
 
   // preparing caches queries
   const query2 = "SELECT * FROM loopdata_by_detector where detectorid = ? and starttime >= ? and starttime < ?";
-  const params2 = [detectorid, start, end];
-
+  const params2 = [detectorid, startDate, endDate];
+  console.log(params2);
   // return rows as is, parse on client
   client.execute(query2, params2, {prepare: true}, function(err, rows) {
 	if(rows.length != 0) {
@@ -108,10 +111,16 @@ app.get('/map/*', function (req, res) {
   });
 });
 
-app.get('/q3', function (req, res) {
+app.get('/query', function (req, res) {
   res.sendFile(path.join(__dirname + "/freeway_loopdata_OneHour.json"));
 });
 
 app.listen(config.server.port, function () {
   console.log(`Project listening on port ${config.server.port}`);
 });
+
+function addDays(dateZero, days) {
+  retDate = new Date(Number(dateZero));
+  retDate.setUTCDate(dateZero.getDate() + days);
+  return retDate;
+}
